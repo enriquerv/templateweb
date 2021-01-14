6ac1c7af0b91718fd8276c48a7880c1241a70c56
Enrique Rodriguez <enrique.rv0704@gmail.com>
2021-01-14 01:26:57 -0600

    External resources, Blades and Controllers configured

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -39,7 +39,9 @@
         {{-- Bootstrap v5.0 JS --}}
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
-        
+        {{-- FontAwesome Script --}}
+        <script src="https://kit.fontawesome.com/286437046b.js" crossorigin="anonymous"></script>
+
         {{-- Custom Script --}}
         <script src="{{ env('APP_URL') }}/assets/js/custom.js"></script>
         
